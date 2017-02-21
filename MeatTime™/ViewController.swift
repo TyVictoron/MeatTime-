11c3ae6f55bef03045aa72132d6c9e9b99a7cd34
Ty Victorson <trv17@ymail.com>
2017-02-21 09:58:12 -0600

    Initial Commit

---
+++ MeatTime™/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeatTime™
+//
+//  Created by Ty Victorson on 2/21/17.
+//  Copyright © 2017 Ty and Ler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MeatTime™Tests/MeatTime_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MeatTime_Tests.swift
+//  MeatTime™Tests
+//
+//  Created by Ty Victorson on 2/21/17.
+//  Copyright © 2017 Ty and Ler. All rights reserved.
+//
+
+import XCTest
+@testable import MeatTime_
+
+class MeatTime_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
