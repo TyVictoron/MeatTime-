20df691e9e659a1daf63e908520db1f979ccfccb
TylerSiemonOfficial <tylersiemon@tylersiemon.local>
2017-02-22 09:41:05 -0600

    view controllers
    set up

---
+++ MeatTime™/MeatLocker.swift
@@ -0,0 +1,14 @@
+//
+//  MeatLocker.swift
+//  MeatTime™
+//
+//  Created by tyler siemon on 2/22/17.
+//  Copyright © 2017 Ty and Ler. All rights reserved.
+//
+
+import UIKit
+
+class MeatLocker: NSObject
+{
+
+}
